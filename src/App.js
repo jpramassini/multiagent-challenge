f2fb352e674a9a2604a56a3f12b5a305d9edb1c4
JP Ramassini <jpramassini@gmail.com>
2019-05-08 15:57:53 -0400

    Added basic graph rendering and input fields to determine n and k.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Graph } from "react-d3-graph";
+import NumericInput from "react-numeric-input";
+import { graphConfig } from "./configs.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+const data = {
+  nodes: [{ id: "Harry" }, { id: "Sally" }, { id: "Alice" }, { id: "JP" }],
+  links: [
+    { source: "Harry", target: "Sally" },
+    { source: "Harry", target: "Alice" },
+    { source: "JP", target: "Alice" }
+  ]
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      n: 7,
+      k: (7 * (7 - 1)) / 2,
+      maxEdges: (7 * (7 - 1)) / 2,
+      data: {
+        nodes: [],
+        links: []
+      },
+      nodeConnections: {}
+    };
+    //this.setNodesCount = this.setNodesCount.bind(this);
+  }
+
+  setNodesCount = newNum => {
+    console.log(newNum);
+    let maxEdges = (newNum * (newNum - 1)) / 2;
+    this.setState({ n: newNum, maxEdges });
+    if (this.state.k > maxEdges) {
+      console.log("New k: " + maxEdges);
+      this.setState({ k: maxEdges });
+    }
+  };
+
+  setEdgeCount = newNum => {
+    console.log("New K: " + newNum);
+    this.setState({ k: newNum });
+  };
+
+  render() {
+    console.log(this.state);
+    if (this.state.data.nodes) {
+      return (
+        <div className="App">
+          <Graph id="graph-id" data={data} config={graphConfig} />
+          <div>
+            <NumericInput
+              min={2}
+              max={100}
+              value={this.state.n}
+              strict
+              onChange={this.setNodesCount}
+            />
+            <NumericInput
+              min={1}
+              max={this.state.maxEdges}
+              value={this.state.k}
+              strict
+              onChange={this.setEdgeCount}
+            />
+          </div>
+        </div>
+      );
+    } else {
+      return <div />;
+    }
+  }
 }
 
 export default App;

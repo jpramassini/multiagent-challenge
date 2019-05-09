780c5e896a2d7831656395a1555b193521435fc5
JP Ramassini <jpramassini@gmail.com>
2019-05-08 21:52:38 -0400

    Updates

---
+++ precache-manifest.8ffd244fad19a7bdb6a581cf386ad767.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e419005c67c40fc3f6b8428d630444",
+    "url": "/multiagent-challenge/index.html"
+  },
+  {
+    "revision": "47a78317d704ca705183",
+    "url": "/multiagent-challenge/static/css/main.0df74ab7.chunk.css"
+  },
+  {
+    "revision": "ea2e8a51828f228ab38b",
+    "url": "/multiagent-challenge/static/js/2.f82c858c.chunk.js"
+  },
+  {
+    "revision": "47a78317d704ca705183",
+    "url": "/multiagent-challenge/static/js/main.56053529.chunk.js"
+  },
+  {
+    "revision": "e968b38e0facc890188b",
+    "url": "/multiagent-challenge/static/js/runtime~main.bd87e27a.js"
+  }
+]);
